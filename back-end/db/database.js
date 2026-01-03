82fbb4b813eee8800ceabd7e4e5d6685374d4f15
oniro108 <mezzomotheo@gmail.com>
2026-01-03 16:44:14 -0300

    db_port adicionado

--- back-end/db/database.js
+++ back-end/db/database.js
@@ -6,7 +6,7 @@ const dbConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: 17102,
+    port: Number(process.env.DB_PORT),
     ssl: {
         rejectUnauthorized: false
     },
