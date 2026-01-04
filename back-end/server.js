03b2de530826fc7879696c56042382b0610b6854
oniro108 <mezzomotheo@gmail.com>
2026-01-03 21:16:39 -0300

    fix: https em cadastro e login, em server js credentials = false -> true

--- back-end/server.js
+++ back-end/server.js
@@ -9,14 +9,11 @@ const PORT = 3000;
 // config do cors pro live server na porta 5500
 app.use(cors({
     origin: [
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
         "https://onirotalk.vercel.app",
-
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: false
+    credentials: true
 }));
 
 app.use(express.json({limit: '10mb'}));
